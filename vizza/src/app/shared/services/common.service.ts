9859913430d1390cf87034ef9c7e0f734a7b0b11
AravinthBala <ssaravinth94@gmail.com>
2018-07-16 09:56:12 +0530

    updated

--- vizza/src/app/shared/services/common.service.ts
+++ vizza/src/app/shared/services/common.service.ts
@@ -87,7 +87,17 @@ export class CommonService {
             .map(this.extractData)
             .catch(this.handleError);
     }
-
+    contactDetails(data) {
+        const json = JSON.stringify(data);
+        const token = this.authService.getAccessToken();
+        const httpOptions = {
+            headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+        };
+        const url = this.configurationService.getHost() + 'v1/contact/addContactDetails' ;
+        return this.http.post(url, json, httpOptions)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
     private extractData(res: Response) {
         const body = res;
         return body || {};
