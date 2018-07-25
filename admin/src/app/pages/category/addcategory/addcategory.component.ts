6ac89a42b4488fc6a105992b4a427df70d51ba63
AravinthBala <ssaravinth94@gmail.com>
2018-07-25 18:06:24 +0530

    Updated

--- admin/src/app/pages/category/addcategory/addcategory.component.ts
+++ admin/src/app/pages/category/addcategory/addcategory.component.ts
@@ -64,9 +64,9 @@ export class AddcategoryComponent implements OnInit {
         if (successData.IsSuccess) {
             this.dialogRef.close(true);
             this.response = successData.ResponseObject;
-            this.toastr.success(successData.ResponseObject);
+            this.toastr.success(successData.ResponseObject,'Added Successfully');
         }else {
-            this.toastr.error(successData.ResponseObject);
+            this.toastr.error(successData.ErrorObject,'Failed');
         }
     }
     public getCategoryFailure(error) {
