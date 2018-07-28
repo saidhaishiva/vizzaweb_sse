36ee4a1a4a6b9c93279ce490d9e04e163646c12b
gnanamani <gnanamani894@gmail.com>
2018-07-28 16:47:15 +0530

    updated

--- admin/src/app/shared/services/common.service.ts
+++ admin/src/app/shared/services/common.service.ts
@@ -40,6 +40,18 @@ export class CommonService {
             .map(this.extractData )
             .catch(this.handleError);
     }
+    getNotes(data) {
+        const json = JSON.stringify(data);
+        console.log(json);
+        const token = this.authService.getAccessToken();
+        const httpOptions = {
+            headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Accesstoken': token})
+        };
+        const url = this.configurationService.getHost() + 'admin/list_allpos_messages';
+        return this.http.post(url , json, httpOptions)
+            .map(this.extractData )
+            .catch(this.handleError);
+    }
     updateVerification(data) {
         const json = JSON.stringify(data);
         console.log(json);
