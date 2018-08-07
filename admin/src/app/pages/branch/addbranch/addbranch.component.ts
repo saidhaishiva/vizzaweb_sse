435035e64683577d9a29b6ba193cfbcb7faf9b63
padmadevil <padmamca.94@gmail.com>
2018-08-07 10:29:10 +0530

    updated branch

--- admin/src/app/pages/branch/addbranch/addbranch.component.ts
+++ admin/src/app/pages/branch/addbranch/addbranch.component.ts
@@ -17,18 +17,26 @@ public settings: any;
     public response: any;
     public status: any;
     loadingIndicator: boolean = true;
+    active: any;
 
     constructor(public fb: FormBuilder, public auth: AuthService, public dialogRef: MatDialogRef<AddbranchComponent>, private toastr: ToastrService, public appSettings: AppSettings, public branchservice: BranchService,
               @Inject(MAT_DIALOG_DATA) public data: any) {
+        this.active = [
+            {value: '0', name:'Inactive'},
+            {value: '1', name:'Active'}
+        ];
         this.form = this.fb.group({
             // 'branchid': ['', Validators.compose([Validators.required])],
             'branchname': ['', Validators.compose([Validators.required, ])],
+            'active': ['', Validators.compose([Validators.required, ])]
 
         });
 
     }
 
   ngOnInit() {
+      this.form.controls['active'].setValue(this.active[1].value);
+
   }
     close(): void {
         this.dialogRef.close();
@@ -40,8 +48,12 @@ public settings: any;
                 'roleid': this.auth.getAdminRoleId(),
                 'userid': this.auth.getAdminId(),
                  'branch_id': '',
-                'branch_name':this.form.controls['branchname'].value
+                'branch_name':this.form.controls['branchname'].value,
+                'branchstatus': this.form.controls['active'].value
+
             };
+            alert();
+            console.log(data,'aaa');
             this.loadingIndicator = true;
             this.branchservice.addbranch(data).subscribe(
                 (successData) => {
