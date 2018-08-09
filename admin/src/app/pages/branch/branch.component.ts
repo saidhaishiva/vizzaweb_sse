5bd4853f7d3341a181c5bad232168865d52876f8
padmadevil <padmamca.94@gmail.com>
2018-08-09 12:46:42 +0530

    updated branch

--- admin/src/app/pages/branch/branch.component.ts
+++ admin/src/app/pages/branch/branch.component.ts
@@ -93,4 +93,16 @@ export class BranchComponent implements OnInit {
         });
 
     }
+
+    updateFilter(event) {
+
+        const val = event.target.value.toLowerCase();
+        console.log(val,'ss');
+        const temp = this.temp.filter(function(d) {
+            return d.branchname.toLowerCase().indexOf(val) !== -1 || !val;
+        });
+        this.rows = temp;
+        console.log(this.rows, 'opo');
+        this.table.offset = 0;
+    }
 }
