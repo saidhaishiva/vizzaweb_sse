9666bf5ee9468e1f295b09a0da9a2eca143a9c9e
gnanamani <gnanamani894@gmail.com>
2018-09-10 17:26:41 +0530

    updated

--- admin/src/app/shared/services/common.service.ts
+++ admin/src/app/shared/services/common.service.ts
@@ -59,7 +59,7 @@ export class CommonService {
         const httpOptions = {
             headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Accesstoken': token})
         };
-        const url = this.configurationService.getHost() + 'posquestion/get_pos_exam_attended_details' ;
+        const url = this.configurationService.getHost() + 'admin/get_pos_exam_attended_details' ;
         return this.http.post(url, json, httpOptions)
             .map(this.extractData)
             .catch(this.handleError);
@@ -77,6 +77,18 @@ export class CommonService {
             .catch(this.handleError);
     }
     getNotes(data) {
+        const json = JSON.stringify(data);
+        console.log(json);
+        const token = this.authService.getAccessToken();
+        const httpOptions = {
+            headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Accesstoken': token})
+        };
+        const url = this.configurationService.getHost() + 'admin/list_allpos_messages';
+        return this.http.post(url , json, httpOptions)
+            .map(this.extractData )
+            .catch(this.handleError);
+    }
+    getComments(data) {
         const json = JSON.stringify(data);
         console.log(json);
         const token = this.authService.getAccessToken();
