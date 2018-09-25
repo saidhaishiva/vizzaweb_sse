d48d7526a9f90a0e80f8b23bba9c3b5000e760a6
padmadevil <padmamca.94@gmail.com>
2018-09-25 19:33:22 +0530

    update

--- vizza/src/app/pages/proposal/proposal.component.ts
+++ vizza/src/app/pages/proposal/proposal.component.ts
@@ -842,6 +842,28 @@ console.log(value,'fgh');
             }
         }
     }
+    public keyEvent(event: any) {
+        if (event.charCode !== 0) {
+            const pattern =/[0-9a-zA-Z ]/;
+            const inputChar = String.fromCharCode(event.charCode);
+            if (!pattern.test(inputChar)) {
+                event.preventDefault();
+            }
+        }
+        // let specialKeys = new Array();
+        // specialKeys.push(8); //Backspace
+        // specialKeys.push(9); //Tab
+        // specialKeys.push(46); //Delete
+        // specialKeys.push(36); //Home
+        // specialKeys.push(35); //End
+        // specialKeys.push(37); //Left
+        // specialKeys.push(39); //Right
+        // let keyCode = e.keyCode == 0 ? e.charCode : e.keyCode;
+        // let ret = ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122) || (specialKeys.indexOf(e.keyCode) != -1 && e.charCode != e.keyCode));
+        // document.getElementById("error").style.display = ret ? "none" : "inline";
+        // return ret;
+        //
+    }
     public data(event: any) {
         if (event.charCode !== 0) {
             const pattern = /[a-zA-Z\\ ]/;
