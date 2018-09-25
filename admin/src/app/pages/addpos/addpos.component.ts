1140c72fa5c5929ccc40d1b21eabf983aaae9ece
kmt2 <ddeepanraj8@gmail.com>
2018-09-25 17:12:21 +0530

    Update

--- admin/src/app/pages/addpos/addpos.component.ts
+++ admin/src/app/pages/addpos/addpos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA, MatTabChangeEvent} from '@angular/material';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 import { CommonService } from '../../shared/services/common.service';
 import { AuthService } from '../../shared/services/auth.service';
@@ -9,10 +9,31 @@ import { LoginService } from '../../shared/services/login.service';
 import { Router } from '@angular/router';
 import { Settings} from '../../app.settings.model';
 import { AppSettings} from '../../app.settings';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import {DatePipe} from '@angular/common';
+import { emailValidator, matchingPasswords } from '../../theme/utils/app-validators';
+
+export const MY_FORMATS = {
+    parse: {
+        dateInput: 'DD/MM/YYYY',
+    },
+    display: {
+        dateInput: 'DD/MM/YYYY',
+        monthYearLabel: 'MM YYYY',
+        dateA11yLabel: 'DD/MM/YYYY',
+
+        monthYearA11yLabel: 'MM YYYY',
+    },
+};
 
 @Component({
     selector: 'app-addpos',
     templateUrl: './addpos.component.html',
+    providers: [
+        {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+        {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+    ]
 })
 export class AddposComponent implements OnInit {
     public form: FormGroup;
@@ -42,8 +63,19 @@ export class AddposComponent implements OnInit {
     mismatchError: any;
     profile: any;
     chequeleaf: any;
+
+    pincodeErrors : any;
+    selectedIndex: any;
+    public title: any;
+    nectStatus: any;
+    DateValidator: any;
+    roleId: any;
+    img: boolean;
+    public passwordHide: boolean = true;
+    personalCitys: any;
     constructor(public config: ConfigurationService,
-                public fb: FormBuilder,public router: Router, public appSettings: AppSettings,public login: LoginService, public common: CommonService, public auth: AuthService, private toastr: ToastrService) {
+                public fb: FormBuilder,public router: Router, public appSettings: AppSettings,public login: LoginService,
+                public common: CommonService, public auth: AuthService, private toastr: ToastrService, public datepipe: DatePipe) {
         this.settings = this.appSettings.settings;
         this.webhost = this.config.getimgUrl();
         this.selectedtab = 0;
@@ -51,18 +83,29 @@ export class AddposComponent implements OnInit {
         this.dob = '';
         this.dobError = '';
         this.mismatchError = '';
+        // this.settings.HomeSidenavUserBlock = false;
+        this.settings.sidenavIsOpened = false;
+        this.settings.sidenavIsPinned = false;
+        this.webhost = this.config.getimgUrl();
+        this.selectedtab = 0;
+        this.nectStatus = true;
+        this.selectedIndex = 0;
+        this.profile = '';
+        this.img = false;
         this.form = this.fb.group({
             personal: this.fb.group({
                 id: null,
-                firstname: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+                firstname: ['', Validators.compose([Validators.required])],
                 lastname: ['', Validators.compose([Validators.required])],
                 birthday: ['', Validators.compose([Validators.required])],
                 gender: ['', Validators.compose([Validators.required])],
-                referralcode: '',
+                referralconduct: ['', Validators.compose( [ Validators.pattern('[6789][0-9]{9}')])],
+                profile: ['',Validators.compose( [Validators.required])]
+
             }),
             contacts: this.fb.group({
-                email: ['', Validators.compose([Validators.required])],
-                phone1: ['', Validators.compose([Validators.required, Validators.minLength(10)])],
+                email: ['', Validators.compose([Validators.required, Validators.pattern('^(([^<>()[\\]\\\\.,;:\\s@\\\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\\\"]+)*)|(\\\".+\\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$')])],
+                phone1: ['', Validators.compose([Validators.required, Validators.pattern('[6789][0-9]{9}')])],
                 phone2: '',
                 address1: ['', Validators.compose([Validators.required])],
                 address2: '',
@@ -70,18 +113,22 @@ export class AddposComponent implements OnInit {
             }),
             documents: this.fb.group({
                 aadharnumber: ['', Validators.compose([Validators.required])],
-                pannumber: ['', Validators.compose([Validators.required])],
-
+                pannumber: ['', Validators.compose([Validators.required, Validators.pattern('^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$')])],
+                aadharfront: ['',Validators.compose( [Validators.required])],
+                aadharback: ['',Validators.compose( [Validators.required])],
+                pancard: ['',Validators.compose( [Validators.required])]
             }),
             education: this.fb.group({
                 qualification: ['', Validators.compose([Validators.required])],
+                educationdocument:['', Validators.compose( [Validators.required])]
 
             }),
             bankdetails: this.fb.group({
                 bankname: ['', Validators.compose([Validators.required])],
                 bankbranch: ['', Validators.compose([Validators.required])],
                 ifsccode: ['', Validators.compose([Validators.required])],
-                accountnumber: ['', Validators.compose([Validators.required])]
+                accountnumber: ['', Validators.compose([Validators.required])],
+                chequeleaf:['', Validators.compose( [Validators.required])]
             })
         });
         this.profile = '';
@@ -94,7 +141,23 @@ export class AddposComponent implements OnInit {
 
 
     ngOnInit() {
+        this.settings.loadingSpinner = false;
+        this.pincodeErrors = false;
     }
+
+    public tabChanged(tabChangeEvent: MatTabChangeEvent): void {
+        this.selectedIndex = tabChangeEvent.index;
+    }
+
+    public nextStep() {
+        this.selectedIndex += 1;
+    }
+
+    public previousStep() {
+        this.selectedIndex -= 1;
+    }
+
+
     readUrl(event: any, type) {
         this.type = type;
         this.size = event.srcElement.files[0].size;
@@ -160,7 +223,7 @@ export class AddposComponent implements OnInit {
     public fileUploadFailure(error) {
             console.log(error);
     }
-    submit() {
+    submit(value) {
         console.log(this.dob, 'dateeee');
         if (this.aadharfront == '') {
             this.toastr.error('Please upload aadhar front page');
@@ -180,20 +243,20 @@ export class AddposComponent implements OnInit {
                 'admin_id': this.auth.getAdminId(),
                 'admin_roleid': this.auth.getAdminRoleId(),
                 "platform": "web",
-                "pos_hidden_id": "",
-                "pos_referralcode": this.form.controls['referralcode'].value,
-                "pos_firstname": this.form.controls['firstname'].value,
-                "pos_lastname": this.form.controls['lastname'].value,
-                "pos_gender": this.form.controls['gender'].value,
+                "pos_referralcode": this.form.value['personal']['referralconduct'],
+                "pos_firstname": this.form.value['personal']['firstname'],
+                "pos_lastname": this.form.value['personal']['lastname'],
+                "pos_gender": this.form.value['personal']['gender'],
                 "pos_dob": this.dob,
                 "pos_mobileno": this.form.value['contacts']['phone1'],
+                'pos_alternate_mobileno': this.form.value['contacts']['phone2'],
                 "pos_email": this.form.value['contacts']['email'],
                 "pos_address1": this.form.value['contacts']['address1'],
                 "pos_address2": this.form.value['contacts']['address2'],
                 "pos_postalcode": this.form.value['contacts']['pincode'],
                 "pos_aadhar_no": this.form.value['documents']['aadharnumber'],
                 "pos_pan_no": this.form.value['documents']['pannumber'],
-                "pos_profile_img": this.profile,
+                "pos_profile_img": this.profile == undefined ? '' : this.profile,
                 "pos_aadhar_front_img": this.aadharfront,
                 "pos_aadhar_back_img": this.aadharback,
                 "pos_pan_img": this.pancard,
@@ -205,7 +268,7 @@ export class AddposComponent implements OnInit {
                 "branch_name": this.form.value['bankdetails']['bankbranch'],
                 "ifsc_code": this.form.value['bankdetails']['ifsccode']
             };
-            console.log(data);
+            console.log(data, 'sdsdsdsds');
             this.settings.loadingSpinner = true;
             this.login.RegisterPos(data).subscribe(
                 (successData) => {
@@ -221,8 +284,10 @@ export class AddposComponent implements OnInit {
         console.log(successData);
         this.settings.loadingSpinner = false
         if (successData.IsSuccess) {
-            this.toastr.success('Registration Completed', 'Success!!!');
             this.router.navigate(['/pos']);
+            this.toastr.success('Admin Registration Completed', 'Success!!!');
+        }else {
+            this.toastr.error(successData.ErrorObject, 'Failed');
         }
     }
     RegisterPosFailure(error) {
@@ -230,7 +295,7 @@ export class AddposComponent implements OnInit {
         console.log(error);
     }
     checkGender() {
-        if (this.form.controls['gender'].value != '' && this.form.controls['gender'].value != undefined) {
+        if (this.form['controls'].personal['controls']['gender'].value != '' && this.form['controls'].personal['controls']['gender'].value != undefined) {
             this.mismatchError = '';
         } else {
             this.mismatchError = 'Gender is required ';
@@ -248,9 +313,48 @@ export class AddposComponent implements OnInit {
         }
     }
 
-    addEvent(event) {
+    public dobkeyPress(event: any) {
+        if (event.charCode !== 0) {
+            const pattern = /[0-9/\\ ]/;
+            const inputChar = String.fromCharCode(event.charCode);
+            if (!pattern.test(inputChar)) {
+                event.preventDefault();
+            }
+        }
+    }
+
+    public character(event: any) {
+        if (event.charCode !== 0) {
+            const pattern = /[a-zA-Z0-9 ]/;
+            const inputChar = String.fromCharCode(event.charCode);
+            if (!pattern.test(inputChar)) {
+                event.preventDefault();
+            }
+        }
+    }
+
+    ageCalculate(dob) {
+        let mdate = dob.toString();
+        let yearThen = parseInt(mdate.substring(8, 10), 10);
+        let monthThen = parseInt(mdate.substring(5, 7), 10);
+        let dayThen = parseInt(mdate.substring(0, 4), 10);
+        let todays = new Date();
+        let birthday = new Date(dayThen, monthThen - 1, yearThen);
+        let differenceInMilisecond = todays.valueOf() - birthday.valueOf();
+        let year_age = Math.floor(differenceInMilisecond / 31536000000);
+        let res = year_age;
+        console.log(res,'fghjk');
+        if (res >= 18) {
+            this.img = false;
+            this.nectStatus = true;
+        } else {
+            this.img = true;
+            this.nectStatus = false;
+        }
+    }
+
+    addEvent(event, i) {
         if (event.value != null) {
-            console.log(event.value._i,  'kjfhasdjfh');
             let selectedDate = '';
             if (typeof event.value._i == 'string') {
                 const pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
@@ -261,15 +365,35 @@ export class AddposComponent implements OnInit {
                     this.dobError = 'Enter Valid Date';
                 }
                 selectedDate = event.value._i;
-                this.dob = event.value._i;
+                // this.dob = event.value._i;
+                let birth = this.form.value['personal']['birthday'].value;
+                let dob = this.datepipe.transform(event.value, 'y-MM-dd');
+                this.dob = dob;
+                console.log(dob,'dob');
+                if (selectedDate.length == 10) {
+                    this.ageCalculate(dob);
+                } else {
+                    this.img = false;
+
+                }
+
             } else if (typeof event.value._i == 'object') {
-                console.log(event.value._i.date, 'objectttttt');
+
+                this.dob = this.datepipe.transform(event.value, 'y-MM-dd');
+                console.log(selectedDate, 'selectedDate.dob');
+                if (selectedDate.length == 10) {
+                    this.ageCalculate(this.datepipe.transform(event.value, 'y-MM-dd'));
+                } else {
+                    this.img = false;
+
+                }
+
                 this.dobError = '';
                 let date = event.value._i.date;
                 if (date.toString().length == 1) {
-                    date = '0'+date;
+                    date = '0' + date;
                 }
-                let month =  (parseInt(event.value._i.month)+1).toString();
+                let month = (parseInt(event.value._i.month) + 1).toString();
 
                 if (month.length == 1) {
                     month = '0' + month;
@@ -278,6 +402,52 @@ export class AddposComponent implements OnInit {
                 this.dob = date + '-' + month + '-' + year;
             }
         }
+    }
+
+    public data(event: any) {
+        if (event.charCode !== 0) {
+            const pattern = /[a-zA-Z\\ ]/;
+            const inputChar = String.fromCharCode(event.charCode);
+            if (!pattern.test(inputChar)) {
+                event.preventDefault();
+            }
+        }
+    }
+    getPin(pin) {
+        console.log(pin, 'pin');
+        this.pin = pin;
+        const data = {
+            'platform': 'web',
+            'user_id': '0',
+            'role_id': '4',
+            'postalcode': this.pin
+        }
+        if (this.pin.length == 6) {
+            this.common.getPincode(data).subscribe(
+                (successData) => {
+                    this.getPinSuccess(successData);
+                },
+                (error) => {
+                    this.getPinlFailure(error);
+                }
+            );
+        }
+
+
+    }
+    public getPinSuccess(successData) {
+
+        if (successData.IsSuccess) {
+            this.pincodeErrors = false;
+        } else {
+            this.pincodeErrors = true;
+            // this.form['controls'].contacts['controls'].pincode.patchValue('');
+            // this.toastr.error('Invalid pincode');
+
+        }
+    }
+
+    public getPinlFailure(error) {
     }
 
 }
