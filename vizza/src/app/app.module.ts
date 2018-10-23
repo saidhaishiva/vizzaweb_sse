615480fa0001f2a407ea95979579d9dfc29f1833
prabhakaranUI <prabhakaranapril26@gmail.com>
2018-10-23 18:44:09 +0530

    religare

--- vizza/src/app/app.module.ts
+++ vizza/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { DatePipe } from '@angular/common';
 import {ScrollToModule} from 'ng2-scroll-to';
 import {PdfViewerModule} from 'ng2-pdf-viewer';
+import { SlickModule } from 'ngx-slick';
 
 // import { ImageUploadModule } from "angular2-image-upload";
 
@@ -152,6 +153,7 @@ import { DmViewresultComponent } from './pages/dm-exam/dm-viewresult/dm-viewresu
       NgxChartsModule,
       PdfViewerModule,
       ScrollToModule.forRoot(),
+      SlickModule.forRoot(),
       // ImageUploadModule.forRoot(),
       AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDe_oVpi9eRSN99G4o6TwVjJbFBNr58NxE'
