ada9d4a95c2f2a4891f797468ba53f5e09def52c
gnanamani <gnanamani894@gmail.com>
2018-10-24 10:44:26 +0530

    updated

--- admin/src/app/shared/services/branch.service.ts
+++ admin/src/app/shared/services/branch.service.ts
@@ -63,7 +63,7 @@ export class BranchService {
         const httpOptions = {
             headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Accesstoken': token})
         };
-        const url = this.configurationService.getHost() + 'admin/dm_managerList' ;
+        const url = this.configurationService.getHost() + 'dm/dm_managerList' ;
         return this.http.post(url, json, httpOptions)
             .map(this.extractData)
             .catch(this.handleError);
@@ -75,7 +75,7 @@ export class BranchService {
         const httpOptions = {
             headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Accesstoken': token})
         };
-        const url = this.configurationService.getHost() + 'admin/dm_manager_add' ;
+        const url = this.configurationService.getHost() + 'dm/dm_manager_add' ;
         return this.http.post(url, json, httpOptions)
             .map(this.extractData)
             .catch(this.handleError);
@@ -88,7 +88,7 @@ export class BranchService {
         const httpOptions = {
             headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Accesstoken': token})
         };
-        const url = this.configurationService.getHost() + 'admin/dm_manager_edit' ;
+        const url = this.configurationService.getHost() + 'dm/dm_manager_edit' ;
         return this.http.post(url, json, httpOptions)
             .map(this.extractData)
             .catch(this.handleError);
@@ -233,7 +233,7 @@ export class BranchService {
         const httpOptions = {
             headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Accesstoken': token})
         };
-        const url = this.configurationService.getHost() + 'admin/dm_manager_delete' ;
+        const url = this.configurationService.getHost() + 'dm/dm_manager_delete' ;
         return this.http.post(url, json, httpOptions)
             .map(this.extractData)
             .catch(this.handleError);
