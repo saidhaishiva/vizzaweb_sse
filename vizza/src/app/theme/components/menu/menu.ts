ef0af0c9dced5043b2dbae074ac8767f473d6a6b
prabhakaranUI <prabhakaranapril26@gmail.com>
2018-10-24 18:26:33 +0530

    religare

--- vizza/src/app/theme/components/menu/menu.ts
+++ vizza/src/app/theme/components/menu/menu.ts
@@ -102,7 +102,7 @@ export const horizontalMenuItems = [
     new Menu (36, 'About POS', '/about-pos', null, null, null, false, 34),
     new Menu (37, 'POS Login', '/pos', null, null, null, false, 34),
     new Menu (35, 'Contact us', '/contact', null, null, null, false, 39),
-    new Menu (38, 'Careers', '/', null, null, null, false, 39),
+    new Menu (38, 'Careers', '/careers', null, null, null, false, 39),
     new Menu (39, 'About us', '/', null, null, null, true, 0),
     new Menu (45, 'Login', '/', null, null, null, true, 0),
     new Menu (46, 'POS Login', '/pos', null, null, null, false, 45),
