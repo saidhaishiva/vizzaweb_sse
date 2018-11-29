8c8674c4c48e4acde96be9cdacc3b933ccec8679
gnanamanim <chandru9048>
2018-11-29 18:41:06 +0530

    updated

---
+++ vizza/src/app/pages/travel-premium-list/travel-compare/travel-compare.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travel-compare',
+  templateUrl: './travel-compare.component.html',
+  styleUrls: ['./travel-compare.component.scss']
+})
+export class TravelCompareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
