c688ed2e204cd2f3300c04e4663dcedd1b402448
kmt2 <ddeepanraj8@gmail.com>
2018-11-29 18:36:19 +0530

    update

--- vizza/src/app/shared/services/proposal.service.ts
+++ vizza/src/app/shared/services/proposal.service.ts
@@ -191,7 +191,17 @@ export class ProposalService {
     }
 
     //Appollo-Munich
-    getIdProofList(data){
+    getTitleCode(data){
+        const json = JSON.stringify(data);
+        console.log(json);
+        const httpOptions = {
+            headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+        };
+        const url = this.configurationService.getHost() + 'apollomunich/get_tittle';
+        return this.http.post(url , json, httpOptions)
+            .map(this.extractData )
+            .catch(this.handleError);
+    }  getIdProofList(data){
         const json = JSON.stringify(data);
         console.log(json);
         const httpOptions = {
