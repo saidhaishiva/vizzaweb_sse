a18bebeee105089257be99a641eeca7c3972fcd9
padmadevil <padmamca.94@gmail.com>
2018-12-25 10:29:57 +0530

    update

--- vizza/src/app/shared/services/proposal.service.ts
+++ vizza/src/app/shared/services/proposal.service.ts
@@ -465,6 +465,16 @@ export class ProposalService {
             .map(this.extractData )
             .catch(this.handleError);
     }
+    hdfcOccupationList(data) {
+        const json = JSON.stringify(data);
+        const httpOptions = {
+            headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+        };
+        const url = this.configurationService.getHost() + 'Personalaccident_hdfc/occupation_lists';
+        return this.http.post(url , json, httpOptions)
+            .map(this.extractData )
+            .catch(this.handleError);
+    }
     getHdfcCityLists(data) {
         const json = JSON.stringify(data);
         const httpOptions = {
