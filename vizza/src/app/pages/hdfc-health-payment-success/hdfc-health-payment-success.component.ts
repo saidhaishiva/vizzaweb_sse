6cc2dd69376bf953e41751df9dca0f45bba10fda
gnanamanim <chandru9048>
2019-01-09 12:39:25 +0530

    updated

---
+++ vizza/src/app/pages/hdfc-health-payment-success/hdfc-health-payment-success.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hdfc-health-payment-success',
+  templateUrl: './hdfc-health-payment-success.component.html',
+  styleUrls: ['./hdfc-health-payment-success.component.scss']
+})
+export class HdfcHealthPaymentSuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
