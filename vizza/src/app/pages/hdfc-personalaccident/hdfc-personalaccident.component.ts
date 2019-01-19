5d76695a043ad0893116dfc54af464ce64126f93
gnanamanim <chandru9048>
2019-01-19 12:24:30 +0530

    updated

--- vizza/src/app/pages/hdfc-personalaccident/hdfc-personalaccident.component.ts
+++ vizza/src/app/pages/hdfc-personalaccident/hdfc-personalaccident.component.ts
@@ -81,6 +81,7 @@ export class HdfcPersonalaccidentComponent implements OnInit {
         this.settings.sidenavIsOpened = false;
         this.settings.sidenavIsPinned = false;
         this.IsCustomerAcceptedPPCPED = false;
+        this.declaration = false;
         this.arr = [];
         this.webhost = this.config.getimgUrl();
 
