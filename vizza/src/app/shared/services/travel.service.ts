195c97520ea87d584e85c809fc19fc9538758953
padmadevil <padmamca.94@gmail.com>
2019-02-05 15:30:17 +0530

    update

--- vizza/src/app/shared/services/travel.service.ts
+++ vizza/src/app/shared/services/travel.service.ts
@@ -54,7 +54,7 @@ export class TravelService {
         const httpOptions = {
             headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
         };
-        const url = this.configurationService.getHostTravel() + 'starhealth/get_premium_details' ;
+        const url = this.configurationService.getHostTravel() + 'productlist/get_premium_details' ;
         return this.http.post(url, json, httpOptions)
             .map(this.extractData)
             .catch(this.handleError);
