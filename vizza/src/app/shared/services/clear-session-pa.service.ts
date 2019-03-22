1d8aedbfa75f21c39e3ad4c1d13c7946be644a67
padmadevil <padmamca.94@gmail.com>
2019-03-22 15:18:58 +0530

    update

--- vizza/src/app/shared/services/clear-session-pa.service.ts
+++ vizza/src/app/shared/services/clear-session-pa.service.ts
@@ -27,19 +27,17 @@ export class ClearSessionPaService {
     sessionStorage.insureoccupationDescription = '';
     sessionStorage.insureoccupationClass = '';
     // home
-    sessionStorage.pincoceP = '';
-    sessionStorage.occupationP = '';
-    sessionStorage.AnnualIncomeP = '';
-    sessionStorage.setAgeP = '';
-    sessionStorage.selectedProfession = '';
-    sessionStorage.setPageP  = '';
-    sessionStorage.enquiryDetailsPa = '';
-    sessionStorage.setAllProductLists = '';
-    sessionStorage.filterCompany = '';
-    sessionStorage.allProductLists = '';
-    sessionStorage.sumInsuredAmountLists = '';
-    sessionStorage.sideMenuP = '';
-    sessionStorage.buyProductsPa = '';
+    // sessionStorage.pincoceP = '';
+    // sessionStorage.occupationP = '';
+    // sessionStorage.AnnualIncomeP = '';
+    // sessionStorage.setAgeP = '';
+    // sessionStorage.selectedProfession = '';
+    // sessionStorage.enquiryDetailsPa = '';
+    // sessionStorage.setAllProductLists = '';
+    // sessionStorage.filterCompany = '';
+    // sessionStorage.allProductLists = '';
+    // sessionStorage.sumInsuredAmountLists = '';
+    // sessionStorage.buyProductsPa = '';
   }
 
 
