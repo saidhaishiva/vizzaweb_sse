c963766ef1a82a703766b9d3fe44c73a7a21d582
keerthanak1506 <keerthanak1506@gmail.com>
2019-04-02 10:27:37 +0530

    updated

--- vizza/src/app/app.routing.ts
+++ vizza/src/app/app.routing.ts
@@ -123,6 +123,7 @@ import {ReliancePaComponent} from './pages/reliance-pa/reliance-pa.component';
 import {BikePremiumListComponent} from './pages/bike-premium-list/bike-premium-list.component';
 import {TermLifePremiumListComponent} from './pages/term-life-premium-list/term-life-premium-list.component';
 import {BikeShriramProposalComponent} from './pages/bike-shriram-proposal/bike-shriram-proposal.component';
+import {LifeBajajProposalComponent} from './pages/life-bajaj-proposal/life-bajaj-proposal.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -242,7 +243,9 @@ export const routes: Routes = [
             { path: 'reliance-pa', component: ReliancePaComponent, data:{breadcrumb: 'Reliance-pa'}},
             { path: 'bikepremium', component: BikePremiumListComponent, data: { breadcrumb: 'Bike Premium'} },
             { path: 'life-premium-list', component: TermLifePremiumListComponent, data: { breadcrumb: 'Life Premium'} },
-            { path: 'bike-shriram-proposal', component: BikeShriramProposalComponent, data:{ breadcrumb: 'Bike-shriram-proposal'}}
+            { path: 'bike-shriram-proposal', component: BikeShriramProposalComponent, data:{ breadcrumb: 'Bike-shriram-proposal'}},
+            { path: 'life-bajaj-proposal', component: LifeBajajProposalComponent, data:{ breadcrumb: 'life-bajaj-proposal'}}
+
         ]
     },
     { path: 'login', loadChildren: 'app/pages/login/login.module#LoginModule' },
