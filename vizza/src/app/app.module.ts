572b88b57ca72be0ac12f0afd520a05fd7f9278d
padmadevil <padmamca.94@gail.com>
2019-04-16 13:32:49 +0530

    update

--- vizza/src/app/app.module.ts
+++ vizza/src/app/app.module.ts
@@ -262,6 +262,8 @@ import { LifeBajajProposalComponent} from './pages/life-bajaj-proposal/life-baja
 import { BikeShriramProposalComponent} from './pages/bike-shriram-proposal/bike-shriram-proposal.component';
 import{ ClearSessionTravelService} from './shared/services/clear-session-travel.service';
 import { ShriramMotorPaymentSuccessComponent } from './pages/shriram-motor-payment-success/shriram-motor-payment-success.component';
+import { HelloComponent } from './hello/hello.component';
+import { RoyalSundaramProposalComponent } from './pages/royal-sundaram-proposal/royal-sundaram-proposal.component';
 
 @NgModule({
   imports: [
@@ -482,7 +484,9 @@ import { ShriramMotorPaymentSuccessComponent } from './pages/shriram-motor-payme
       TermLifePremiumListComponent,
       LifeBajajProposalComponent,
       BikeShriramProposalComponent,
-      ShriramMotorPaymentSuccessComponent
+      ShriramMotorPaymentSuccessComponent,
+      HelloComponent,
+      RoyalSundaramProposalComponent
 
   ],
   providers: [
---
+++ vizza/src/app/pages/royal-sundaram-proposal/royal-sundaram-proposal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-royal-sundaram-proposal',
+  templateUrl: './royal-sundaram-proposal.component.html',
+  styleUrls: ['./royal-sundaram-proposal.component.scss']
+})
+export class RoyalSundaramProposalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
