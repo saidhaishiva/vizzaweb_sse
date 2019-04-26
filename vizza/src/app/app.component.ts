0fea18e4cb7ea8c1de9833556380838a11ffc135
prabhakaranui <prabhakaranapril26@gmail.com>
2019-04-26 13:12:57 +0530

    UPDATE

--- vizza/src/app/app.component.ts
+++ vizza/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild} from '@angular/core';
+import {Component, HostListener, ViewChild} from '@angular/core';
 import { AppSettings } from './app.settings';
 import { Settings } from './app.settings.model';
 
@@ -9,10 +9,24 @@ import { Settings } from './app.settings.model';
 })
 export class AppComponent {
   public settings: Settings;
+  public mobileView: any;
   constructor(public appSettings:AppSettings){
       this.settings = this.appSettings.settings;
+      this.mobileView = true;
       console.log(this.settings);
+
   } 
 
   ngOnInit() { }
+
+
+@HostListener('window:resize')
+   public onWindowResize():void {
+    if (window.innerWidth <= 992 && window.innerHeight < window.innerWidth ) {
+        this.mobileView = false;
+    } else{
+        this.mobileView = true;
+    }
+}
+
 }
