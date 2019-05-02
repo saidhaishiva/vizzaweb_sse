fab1cac0b1c5605819f76bc4b8378f21aa84a12d
gnanamanim <chandru9048>
2019-05-02 11:29:40 +0530

    updated

--- vizza/src/app/pages/travel-home/travel-home.component.ts
+++ vizza/src/app/pages/travel-home/travel-home.component.ts
@@ -130,6 +130,7 @@ export class TravelHomeComponent implements OnInit {
         sessionStorage.travelUserType = this.travelUserType;
         let today = new Date();
         this.today = new Date(today.getFullYear(), today.getMonth(), today.getDate() +1);
+        this.endDate = this.today;
 
         this.fireapp = this.fb.group({
             'appdate': ['', Validators.required],
@@ -260,7 +261,7 @@ export class TravelHomeComponent implements OnInit {
         this.travelPlan = '';
         this.duration = '';
         this.startDate = '';
-        this.endDate = '';
+        this.endDate = this.today;
         this.daysCount = '';
         this.maxDate = '';
         this.medicalCondition = '';
