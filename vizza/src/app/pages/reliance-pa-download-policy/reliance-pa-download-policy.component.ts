6bff4d0a7e9bb92250d8d71d6f31da658303bdce
sujayap7s <sujayap7s@gmail.com>
2019-05-15 19:25:10 +0530

    updated

--- vizza/src/app/pages/reliance-pa-download-policy/reliance-pa-download-policy.component.ts
+++ vizza/src/app/pages/reliance-pa-download-policy/reliance-pa-download-policy.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {PersonalAccidentService} from '../../shared/services/personal-accident.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import {AuthService} from '../../shared/services/auth.service';
+import {Settings} from '../../app.settings.model';
+import { AppSettings } from '../../app.settings';
+import {ConfigurationService} from '../../shared/services/configuration.service';
 
 @Component({
   selector: 'app-reliance-pa-download-policy',
@@ -7,9 +13,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReliancePaDownloadPolicyComponent implements OnInit {
 
-  constructor() { }
+  public settings: Settings;
+  public proposalId: any;
+  public paymentStatus: any;
+  type: any;
+  currenturl: any;
+  path: any;
 
-  ngOnInit() {
+  constructor(public config: ConfigurationService, public personalservice: PersonalAccidentService, public route: ActivatedRoute, public appSettings: AppSettings, public auth: AuthService) {
+    this.route.params.forEach((params) => {
+      this.proposalId = params.proId;
+    });
+
   }
 
+  ngOnInit() {
+    this.DownloadPdf();
+  }
+
+  DownloadPdf() {
+    const data = {
+      'mail_status': '0',
+      'proposal_id': this.proposalId,
+      'platform': 'web',
+      'user_id': this.auth.getPosUserId() ? this.auth.getPosUserId() : '0',
+      'role_id': this.auth.getPosRoleId() ? this.auth.getPosRoleId() : '4',
+    }
+    this.personalservice.getReliancePaDownloadpdf(data).subscribe(
+        (successData) => {
+          this.downloadPdfSuccess(successData);
+        },
+        (error) => {
+          this.downloadPdfFailure(error);
+        }
+    );
+  }
+
+  public downloadPdfSuccess(successData) {
+    this.settings.loadingSpinner = false;
+    console.log(successData.ResponseObject, 'ssssssssssssssssssssss');
+    if (successData.IsSuccess == true) {
+      this.type = successData.ResponseObject.type;
+      this.path = successData.ResponseObject.path;
+      this.currenturl = this.config.getimgUrl();
+      if (this.type == 'pdf') {
+        console.log(this.currenturl);
+        console.log(successData.ResponseObject.path, 'path');
+        this.path = successData.ResponseObject.path;
+        window.open(this.currenturl + '/' + this.path, '_blank');
+      }
+    }
+  }
+
+  public downloadPdfFailure(error) {
+    this.settings.loadingSpinner = false;
+    console.log(error);
+  }
 }
