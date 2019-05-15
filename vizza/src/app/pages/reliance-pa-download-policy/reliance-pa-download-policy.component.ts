0a1dd7aee8cf4e1d79acb6ac6e1c0a938b2806ad
sujayap7s <sujayap7s@gmail.com>
2019-05-15 16:27:35 +0530

    updated

---
+++ vizza/src/app/pages/reliance-pa-download-policy/reliance-pa-download-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reliance-pa-download-policy',
+  templateUrl: './reliance-pa-download-policy.component.html',
+  styleUrls: ['./reliance-pa-download-policy.component.scss']
+})
+export class ReliancePaDownloadPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
