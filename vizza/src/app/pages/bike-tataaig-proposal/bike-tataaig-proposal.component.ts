d816a0a6e344caf72ce3a5fec91debd2381d538f
sujayap7s <sujayap7s@gmail.com>
2019-05-24 18:49:16 +0530

    new component directory

---
+++ vizza/src/app/pages/bike-tataaig-proposal/bike-tataaig-proposal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-tataaig-proposal',
+  templateUrl: './bike-tataaig-proposal.component.html',
+  styleUrls: ['./bike-tataaig-proposal.component.scss']
+})
+export class BikeTataaigProposalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
