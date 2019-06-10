3db0cfcc07cf9eb3213b7effd639c6873f6c407e
sujayap7s <sujayap7s@gmail.com>
2019-06-10 15:47:58 +0530

    updated

--- vizza/src/app/shared/services/bike-insurance.service.ts
+++ vizza/src/app/shared/services/bike-insurance.service.ts
@@ -686,7 +686,7 @@ export class BikeInsuranceService {
     const httpOptions = {
       headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
     };
-    const url = this.configurationService.getBikeInsurance() + 'tata/financerDetails';
+    const url = this.configurationService.getBikeInsurance() + 'tata/PolicyDownload';
     return this.http.post(url,json, httpOptions)
         .map(this.extractData)
         .catch(this.handleError);
