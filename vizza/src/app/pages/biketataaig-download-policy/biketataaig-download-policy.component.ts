c1f139b277dca264e0dfaaa4b3954481fa98dac2
sujayap7s <sujayap7s@gmail.com>
2019-06-10 11:09:36 +0530

    component added

---
+++ vizza/src/app/pages/biketataaig-download-policy/biketataaig-download-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-biketataaig-download-policy',
+  templateUrl: './biketataaig-download-policy.component.html',
+  styleUrls: ['./biketataaig-download-policy.component.scss']
+})
+export class BiketataaigDownloadPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
