2f0a711321724196bf5b2d5ea3cb749192a8fe00
keerthanak1506 <keerthanak1506@gmail.com>
2019-06-10 18:48:17 +0530

    update

--- vizza/src/app/pages/aegon-term-life/aegon-term-life.component.ts
+++ vizza/src/app/pages/aegon-term-life/aegon-term-life.component.ts
@@ -17,6 +17,7 @@ import { Pipe, PipeTransform, Inject, LOCALE_ID } from '@angular/core';
 import {ValidationService} from '../../shared/services/validation.service';
 import {ActivatedRoute} from '@angular/router';
 import * as moment from 'moment';
+import {TermLifeCommonService} from '../../shared/services/term-life-common.service';
 
 
 export const MY_FORMATS = {
@@ -45,11 +46,48 @@ export class AegonTermLifeComponent implements OnInit {
   public personal: FormGroup;
   public nominee: FormGroup;
   public proposerAge: any;
+  public nomineeAge: any;
   public dateError: any;
+  public dateError1: any;
+  public dateError2: any;
   public today: any;
+  public summaryData: any;
+  public proposerFormData: any;
+  public nomineeFormData: any;
+  public stepper1: any;
+  public personalData: any;
+  public nomineeData: any;
+  public appointeeAge: any;
+  public proposalId: any;
+  public settings: Settings;
+  public stepper2: any;
+  public inputReadonly: boolean;
+  public apponiteeList: boolean;
 
 
-  constructor(public validation: ValidationService, public fb: FormBuilder, public datepipe: DatePipe, private toastr: ToastrService) {
+
+
+
+
+
+  constructor(public validation: ValidationService, public fb: FormBuilder,public route: ActivatedRoute,public TermLifeService: TermLifeCommonService,public datepipe: DatePipe, private toastr: ToastrService, public appSettings: AppSettings) {
+    let stepperindex = 0;
+    this.route.params.forEach((params) => {
+      if(params.stepper == true || params.stepper == 'true') {
+        stepperindex = 2;
+        if (sessionStorage.summaryData != '' && sessionStorage.summaryData != undefined) {
+          this.summaryData = JSON.parse(sessionStorage.summaryData);
+          // this.RediretUrlLink = this.summaryData.PaymentURL;
+          this.proposerFormData = JSON.parse(sessionStorage.proposerFormData);
+          this.nomineeFormData = JSON.parse(sessionStorage.nomineeFormData);
+          this.proposalId = this.summaryData.ProposalId;
+          sessionStorage.aegon_proposal_id = this.proposalId;
+        }
+      }
+    });
+    this.inputReadonly=false;
+    this.apponiteeList = false;
+    this.settings= this.appSettings.settings;
     this.personal = this.fb.group({
       title: ['', Validators.required],
       firstName: '',
@@ -86,27 +124,27 @@ export class AegonTermLifeComponent implements OnInit {
       cCity: '',
       cState: '',
       cPincode: '',
-      isAddressSame: '',
+      isAddressSame: false,
 
 
     });
     this.nominee = this.fb.group({
-      'ntitle': '',
-      'nfirstName': '',
-      'nlastName': '',
-      'ndob': '',
-      'nRelation': '',
-      'nAddress1': '',
-      'nAddress2': '',
-      'nCity': '',
-      'nState': '',
-      'nPincode': '',
-      'nPercentage': '',
-      'atitle': '',
-      'aFullName': '',
-      'adob': '',
-      'aRelation': '',
-      'aPercentage': '',
+      ntitle: '',
+      nfirstName: '',
+      nlastName: '',
+      ndob: '',
+      nRelation: '',
+      nAddress1: '',
+      nAddress2: '',
+      nCity: '',
+      nState: '',
+      nPincode: '',
+      nPercentage: '',
+      atitle: '',
+      aFullName: '',
+      adob: '',
+      aRelation: '',
+      aPercentage: '',
 
 
     });
@@ -194,6 +232,86 @@ export class AegonTermLifeComponent implements OnInit {
     }
   }
 
+
+
+  addDate(event) {
+    if (event.value != null) {
+      let selectedDate = '';
+      this.nomineeAge = '';
+      let dob = '';
+      if (typeof event.value._i == 'string') {
+        const pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+        if (pattern.test(event.value._i) && event.value._i.length == 10) {
+          this.dateError1 = '';
+        } else {
+          this.dateError1 = 'Enter Valid Date';
+        }
+        selectedDate = event.value._i;
+        dob = this.datepipe.transform(event.value, 'y-MM-dd');
+        if (selectedDate.length == 10) {
+          this.nomineeAge = this.ageCalculate(dob);
+
+        }
+
+      } else if (typeof event.value._i == 'object') {
+        // dob = this.datepipe.transform(event.value, 'MMM d, y');
+        dob = this.datepipe.transform(event.value, 'y-MM-dd');
+        if (dob.length == 10) {
+          this.nomineeAge = this.ageCalculate(dob);
+          // sessionStorage.insuredAgePA = this.proposerAge;
+
+        }
+        this.dateError1 = '';
+      }
+      sessionStorage.nomineeAge = this.nomineeAge;
+
+    }
+  }
+
+  addEvent1(event) {
+    if (event.value != null) {
+      let selectedDate = '';
+      this.appointeeAge = '';
+      let dob = '';
+      if (typeof event.value._i == 'string') {
+        const pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+        if (pattern.test(event.value._i) && event.value._i.length == 10) {
+          this.dateError2 = '';
+        } else {
+          this.dateError2 = 'Enter Valid Date';
+        }
+        selectedDate = event.value._i;
+        dob = this.datepipe.transform(event.value, 'y-MM-dd');
+        if (selectedDate.length == 10) {
+          this.appointeeAge = this.ageCalculate(dob);
+
+        }
+
+      } else if (typeof event.value._i == 'object') {
+        // dob = this.datepipe.transform(event.value, 'MMM d, y');
+        dob = this.datepipe.transform(event.value, 'y-MM-dd');
+        if (dob.length == 10) {
+          this.appointeeAge = this.ageCalculate(dob);
+          // sessionStorage.insuredAgePA = this.proposerAge;
+
+        }
+        this.dateError2 = '';
+      }
+      sessionStorage.nomineeAge = this.nomineeAge;
+
+    }
+  }
+
+  ageNominee() {
+    if(sessionStorage.nomineeAge <= 18){
+      this.apponiteeList = true;
+      console.log(this.apponiteeList,'cccccc')
+    } else {
+      this.apponiteeList = false;
+
+    }
+  }
+
   // AGE VALIDATION
   ageCalculate(dob) {
     let today = new Date();
@@ -207,29 +325,56 @@ export class AegonTermLifeComponent implements OnInit {
     return age;
   }
 
-  sameAddress(evnt) {
-    if (evnt.checked) {
-      this.personal.controls['pAddress1'].patchValue(this.personal.controls['cAddress1'].value);
-      this.personal.controls['pAddress2'].patchValue(this.personal.controls['cAddress2'].value);
-      this.personal.controls['pCity'].patchValue(this.personal.controls['cCity'].value);
-      this.personal.controls['pState'].patchValue(this.personal.controls['cState'].value);
-      this.personal.controls['pPincode'].patchValue(this.personal.controls['cPincode'].value);
-
+  // sameAddress(event) {
+  //   if (event.checked) {
+  //     console.log(event.checked,'checked');
+  //     // this.personal.controls['isAddressSame'].value){
+  //
+  //     this.personal.controls['pAddress1'].patchValue(this.personal.controls['cAddress1'].value);
+  //     this.personal.controls['pAddress2'].patchValue(this.personal.controls['cAddress2'].value);
+  //     this.personal.controls['pCity'].patchValue(this.personal.controls['cCity'].value);
+  //     this.personal.controls['pState'].patchValue(this.personal.controls['cState'].value);
+  //     this.personal.controls['pPincode'].patchValue(this.personal.controls['cPincode'].value);
+  //
+  //
+  //   } else {
+  //     this.personal.controls['pAddress1'].patchValue('');
+  //     this.personal.controls['pAddress2'].patchValue('');
+  //     this.personal.controls['pCity'].patchValue('');
+  //     this.personal.controls['pState'].patchValue('');
+  //     this.personal.controls['pPincode'].patchValue('');
+  //
+  //   }
+  // }
+  // SAME AS ADDRESS
+  sameAddress(value:any){
+    if (this.personal.controls['isAddressSame'].value) {
+      console.log(this.personal.controls['isAddressSame'].value,'ifff');
+      this.inputReadonly = true;
+      this.personal.controls['cAddress1'].patchValue(this.personal.controls['pAddress1'].value);
+      console.log(this.personal.controls['cAddress1'].value,'ytfhfasd');
+          this.personal.controls['cAddress2'].patchValue(this.personal.controls['pAddress2'].value);
+          this.personal.controls['cCity'].patchValue(this.personal.controls['pCity'].value);
+          this.personal.controls['cState'].patchValue(this.personal.controls['pState'].value);
+          this.personal.controls['cPincode'].patchValue(this.personal.controls['pPincode'].value);
 
     } else {
-      this.personal.controls['pAddress1'].patchValue('');
-      this.personal.controls['pAddress2'].patchValue('');
-      this.personal.controls['pCity'].patchValue('');
-      this.personal.controls['pState'].patchValue('');
-      this.personal.controls['pPincode'].patchValue('');
+      this.inputReadonly = false;
+      this.personal.controls['cAddress1'].patchValue('');
+          this.personal.controls['cAddress2'].patchValue('');
+          this.personal.controls['cCity'].patchValue('');
+          this.personal.controls['cState'].patchValue('');
+          this.personal.controls['cPincode'].patchValue('');
 
     }
+
   }
 
 
   // NEXT BUTTON
 
   public personalDetails(stepper: MatStepper, value) {
+    this.personalData=value;
     console.log(value, 'eeeeeeeeeee');
     sessionStorage.stepper1 = '';
     sessionStorage.stepper1 = JSON.stringify(value);
@@ -246,6 +391,24 @@ export class AegonTermLifeComponent implements OnInit {
     }
 
   }
+
+  //NEXT BUTTON NOMINEE
+  public nomineeDetails(stepper: MatStepper, value) {
+    console.log(value, 'nominee');
+    sessionStorage.stepper2 = '';
+    sessionStorage.stepper2 = JSON.stringify(value);
+    console.log(this.nominee.valid, 'checked');
+    if(this.nominee.valid) {
+      this.nomineeData= value;
+    console.log(this.nomineeData,'nomm')
+      this.proposal(stepper);
+
+      // stepper.next();
+      //   this.topScroll();
+      }
+    }
+
+
 
 
   sessionData() {
@@ -315,6 +478,139 @@ export class AegonTermLifeComponent implements OnInit {
 
     }
 
+    if (sessionStorage.aegon_proposal_id != '' && sessionStorage.aegon_proposal_id != undefined) {
+      this.proposalId = sessionStorage.aegon_proposal_id;
+    }
+
 
   }
+
+  // proposal Creation
+
+  proposal(stepper) {
+
+    const data =
+        {
+          "user_id": "0",
+          "role_id": "4",
+          "pos_status": "0",
+          "platform": "web",
+          "product_id": "86",
+          "suminsured_Amount": "5000000",
+          "policy_id": "1",
+          "benefitOption": "LP",
+          "personalInformation": {
+            "tittle": this.personal.controls['title'].value,
+            "firstName": this.personal.controls['firstName'].value,
+            "middleName": this.personal.controls['middleName'].value,
+            "lasteName": this.personal.controls['lastName'].value,
+            "gender": this.personal.controls['gender'].value,
+            "dob": this.datepipe.transform(this.personal.controls['dob'].value,'y-MM-dd'),
+            "mobile": this.personal.controls['mobile'].value,
+            "email": this.personal.controls['email'].value,
+            "fathername": this.personal.controls['fatherName'].value,
+            "maritalStatus":this.personal.controls['maritalStatus'].value,
+            "qualifiction": this.personal.controls['qualifiction'].value,
+            "employeeType": this.personal.controls['employeeType'].value,
+            "natureOfWork": this.personal.controls['natureOfWork'].value,
+            "annualIncome": this.personal.controls['annualIncome'].value,
+            "smoker": this.personal.controls['smoker'].value,
+            "isExistingPolicyHolder": this.personal.controls['isExistingPolicyHolder'].value,
+            "isPoliticleExposed": this.personal.controls['isPoliticleExposed'].value,
+            "diabeteDuration": this.personal.controls['diabeteDuration'].value,
+            "isHousewife": this.personal.controls['isHousewife'].value,
+            "isHusbandCover": this.personal.controls['isHusbandCover'].value
+          },
+          "addressDetail": {
+            "pAddress1": this.personal.controls['pAddress1'].value,
+            "pAddress2": this.personal.controls['pAddress2'].value,
+            "pCity": this.personal.controls['pCity'].value,
+            "pState": this.personal.controls['pState'].value,
+            "pPincode": this.personal.controls['pPincode'].value,
+            "cAddress1": this.personal.controls['cAddress1'].value,
+            "cAddress2": this.personal.controls['cAddress2'].value,
+            "cCity": this.personal.controls['cCity'].value,
+            "cState": this.personal.controls['cState'].value,
+            "cPincode": this.personal.controls['cPincode'].value,
+            "isAddressSame": this.personal.controls['isAddressSame'].value
+          },
+          "nomineeDetail": {
+            "tittle": this.nomineeData.ntitle,
+            "firstName": this.nomineeData.nfirstName,
+            "lastName":  this.nomineeData.nlastName,
+            "dob": this.datepipe.transform(this.nomineeData.ndob,'y-MM-dd'),
+            "relation":  this.nomineeData.nRelation,
+            "address1":  this.nomineeData.nAddress1,
+            "address2":  this.nomineeData.nAddress2,
+            "city": this.nomineeData.nCity,
+            "state":  this.nomineeData.nState,
+            "pincode":  this.nomineeData.nPincode,
+            "percent": this.nomineeData.nPercentage,
+            "appointeeTittle":  this.nomineeData.atitle,
+            "appointeeFullName":  this.nomineeData.aFullName,
+            "appointeeDob":  this.nomineeData.adob,
+            "appointeeRelation":  this.nomineeData.aRelation,
+            "appointeePercent":  this.nomineeData.aPercentage
+          },
+          "addonITerm": {
+            "adbr": "NO",
+            "adbrSumAssured": "0",
+            "idis": "NO",
+            "lumpSumBenefitSA": "0",
+            "icir": "NO",
+            "icirSumAssured": "0",
+            "woprCI": "NO",
+            "wcir": "NO"
+          },
+          "addons_itermplus": {
+            "adbrSumAssured": "",
+            "deathBenefitSA": "434",
+            "deathBenefitTISA": "",
+            "enchancedCISA": "",
+            "icirSumAssured": ""
+          }
+        };
+
+
+
+    console.log(data,'proposal data')
+  //   this.settings.loadingSpinner = true;
+  //   this.TermLifeService.getCholaProposal(data).subscribe(
+  //       (successData) => {
+  //         this.setCholaProposalSuccess(successData, stepper);
+  //       },
+  //       (error) => {
+  //         this.setCholaProposalFailure(error);
+  //       }
+  //   );
+  //
+  // }
+  // public setCholaProposalSuccess(successData, stepper) {
+  //   this.settings.loadingSpinner = false;
+  //   if (successData.IsSuccess == true) {
+  //     stepper.next();
+  //     this.topScroll();
+  //     this.toastr.success('Proposal created successfully!!');
+  //     this.summaryData = successData.ResponseObject;
+  //     sessionStorage.summaryData = JSON.stringify(this.summaryData);
+  //     this.proposalId = this.summaryData.ProposalId;
+  //     this.proposerFormData = this.personal.value;
+  //     this.nomineeFormData = this.nomineeDetails.value;
+  //     this.insuredFormData = this.insurerData;
+  //     sessionStorage.proposerFormData = JSON.stringify(this.proposerFormData);
+  //     sessionStorage.insuredFormData = JSON.stringify(this.insuredFormData);
+  //     sessionStorage.nomineeFormData = JSON.stringify(this.nomineeFormData);
+  //     sessionStorage.chola_health_proposal_id = this.proposalId;
+
+      stepper.next();
+      // this.nextStep();
+
+    // } else {
+    //   this.toastr.error(successData.ErrorObject);
+    // }
+  }
+  // public setCholaProposalFailure(error) {
+  // }
+
+
 }
