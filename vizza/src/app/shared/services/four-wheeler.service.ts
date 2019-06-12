da0a8cc29971cc1271d32408438633b91300496d
SharmilaDeviS <sharmiladevi5795@gmail.com>
2019-06-12 12:15:27 +0530

    update

--- vizza/src/app/shared/services/four-wheeler.service.ts
+++ vizza/src/app/shared/services/four-wheeler.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ConfigurationService} from './configuration.service';
+import {AuthService} from './auth.service';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
+
 export class FourWheelerService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private configurationService: ConfigurationService, private authService: AuthService, public auth: AuthService) {
 
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      // const body = error.json() || '';
+      const err = error || JSON.stringify(error);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    return Observable.throw(error);
+  }
 }
