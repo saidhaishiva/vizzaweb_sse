51b44d77fa8adf1442775fceb54532c867ba1a0b
gnanamanim <chandru9048>
2019-06-12 12:01:21 +0530

    updated

---
+++ vizza/src/app/pages/four-wheeler-home/four-wheeler-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-four-wheeler-home',
+  templateUrl: './four-wheeler-home.component.html',
+  styleUrls: ['./four-wheeler-home.component.scss']
+})
+export class FourWheelerHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
