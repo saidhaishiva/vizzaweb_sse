f87c3555acbeb5e6025f26fd991edb2a85a11756
sujayap7s <sujayap7s@gmail.com>
2019-06-15 11:27:45 +0530

    updated

--- vizza/src/app/shared/services/bike-insurance.service.ts
+++ vizza/src/app/shared/services/bike-insurance.service.ts
@@ -706,6 +706,19 @@ export class BikeInsuranceService {
         .catch(this.handleError);
   }
 
+  //TAtaaig Coverdrivelist
+
+  coverdrive(data) {
+    const json = JSON.stringify(data);
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+    };
+    const url = this.configurationService.getBikeInsurance() + 'tata/driverAddons';
+    return this.http.post(url,json, httpOptions)
+        .map(this.extractData)
+        .catch(this.handleError);
+  }
+
   //
   getDownloadPdfTataaig(data) {
     const json = JSON.stringify(data);
