a8b261623004fd9bd0800d0e2113f0a05c88c4b3
sriramcraz <Sriram.r15>
2019-06-18 17:18:57 +0530

    comiit

---
+++ vizza/src/app/pages/reliance-twowheeler-payment-success/reliance-twowheeler-payment-success.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reliance-twowheeler-payment-success',
+  templateUrl: './reliance-twowheeler-payment-success.component.html',
+  styleUrls: ['./reliance-twowheeler-payment-success.component.scss']
+})
+export class RelianceTwowheelerPaymentSuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
