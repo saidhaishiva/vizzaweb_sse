24f4aa8e093cdacd62b28625ee6b88b78019a9c8
sujay <sujayap7s@gmail.com>
2019-07-03 15:34:10 +0530

    updated

---
+++ admin/src/app/pages/testimonial/addtestimonial/addtestimonial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addtestimonial',
+  templateUrl: './addtestimonial.component.html',
+  styleUrls: ['./addtestimonial.component.scss']
+})
+export class AddtestimonialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ admin/src/app/pages/testimonial/edittestimonial/edittestimonial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edittestimonial',
+  templateUrl: './edittestimonial.component.html',
+  styleUrls: ['./edittestimonial.component.scss']
+})
+export class EdittestimonialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
