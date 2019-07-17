64dd8dedf69aca2810854ee45a568570cf0d4282
padmadevil <padmamca.94@gail.com>
2019-07-17 15:59:13 +0530

    update

--- admin/src/app/pages/career-list/path-popup/path-popup.component.ts
+++ admin/src/app/pages/career-list/path-popup/path-popup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {BranchService} from '../../../shared/services/branch.service';
 
 @Component({
   selector: 'app-path-popup',
@@ -8,17 +9,16 @@ import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MAT_DIALOG_DATA, MatDial
   styleUrls: ['./path-popup.component.scss']
 })
 export class PathPopupComponent implements OnInit {
-public form : FormGroup
-  constructor(public fb : FormBuilder,   public dialogRef: MatDialogRef<PathPopupComponent>,
+public form : FormGroup;
+  constructor(public fb : FormBuilder,  public branchservice: BranchService, public dialogRef: MatDialogRef<PathPopupComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) {
   this.form = this.fb.group({
-    name:'',
-    email:'',
-    mobile: '',
-    status:'',
-    createdDate: '',
+    email: ['', Validators.compose([Validators.required, Validators.pattern('^(([^<>()[\\]\\\\.,;:\\s@\\\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\\\"]+)*)|(\\\".+\\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$')])],
+    subject: '',
+    message: ''
 
   })
+
   }
 
   ngOnInit() {
@@ -26,4 +26,30 @@ public form : FormGroup
   close(): void {
     this.dialogRef.close();
   }
+  submit(){
+    this.dialogRef.close();
+      const data = {
+        'platform': 'web',
+      };
+      this.branchservice.scheduleDetails(data).subscribe(
+          (successData) => {
+            this.ScheduleSuccess(successData);
+          },
+          (error) => {
+            this.ScheduleFailure(error);
+          }
+      );
+    }
+
+  public ScheduleSuccess(success) {
+      console.log(success);
+      if (success.IsSuccess) {
+        let details = success.ResponseObject;
+      }
+
+  }
+  public ScheduleFailure(error){
+
+  }
+
 }
