3f709ee21df258f3f34d4200ed1a70caf0bf15f5
sujay <sujayap7s@gmail.com>
2019-07-23 11:19:47 +0530

    updated

--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -46,7 +46,6 @@ import {AddcenterComponent} from './pages/mediacenter/addcenter/addcenter.compon
 import {EditmediaComponent} from './pages/mediacenter/editmedia/editmedia.component';
 import { TestimonialComponent} from './pages/testimonial/testimonial.component';
 import {CareerListComponent} from './pages/career-list/career-list.component';
-import { TestimonialmanagerComponent } from './pages/testimonialmanager/testimonialmanager.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -87,10 +86,8 @@ export const routes: Routes = [
             { path: 'dm-add', component: AddDmComponent, data: { breadcrumb: 'Add DM' } },
             { path: 'dm-edit/:id', component: EditDmComponent, data: { breadcrumb: 'Edit DM' } },
             { path: 'dm-profile/:id/:status', component: DmProfileComponent, data: { breadcrumb: 'DM Profile' } },
-            { path: 'testimonial', component: TestimonialComponent, data: { breadcrumb: 'Testimoinial'} },
+            { path: 'testimonial', component: TestimonialComponent, data: { breadcrumb: 'Testimonial'} },
             { path: 'careerList', component: CareerListComponent, data: { breadcrumb: 'Career'} },
-            { path: 'testimonialmanager', component: TestimonialmanagerComponent, data: { breadcrumb: 'Testimonialmanager'} }
-
         ]
     },
     { path: 'login',  loadChildren: 'app/pages/login/login.module#LoginModule' },
