4204c59a6d2796a39ab8bacffb629bce7a7ba203
maniVizza <gnanamani.m@vizzainsurance.com>
2019-07-30 15:10:58 +0530

    updated

--- vizza/src/app/shared/services/bike-insurance.service.ts
+++ vizza/src/app/shared/services/bike-insurance.service.ts
@@ -882,6 +882,17 @@ export class BikeInsuranceService {
         .map(this.extractData)
         .catch(this.handleError);
   }
+    proposalHdfc(data) {
+    const json = JSON.stringify(data);
+    const token = this.authService.getAccessToken();
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+    };
+    const url = this.configurationService.getBikeInsurance() + 'hdfc/proposal';
+    return this.http.post(url,json, httpOptions)
+        .map(this.extractData)
+        .catch(this.handleError);
+  }
 
 
   private handleError(error: Response | any) {
