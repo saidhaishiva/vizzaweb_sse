5be072178d570e7c8d1d51a4c0c4fd4fbb24f3e4
sujay <sujayap7s@gmail.com>
2019-08-23 16:50:22 +0530

    updated

---
+++ vizza/src/app/edelweisspos-premium-list/edelweisspos-premium-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edelweisspos-premium-list',
+  templateUrl: './edelweisspos-premium-list.component.html',
+  styleUrls: ['./edelweisspos-premium-list.component.scss']
+})
+export class EdelweissposPremiumListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
