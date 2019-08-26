7867190e16e3ee662f823c537fff8a68b67a1e3a
sujay <sujayap7s@gmail.com>
2019-08-26 15:35:43 +0530

    componedt added

---
+++ vizza/src/app/edelweiss-pos-home/edelweiss-pos-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edelweiss-pos-home',
+  templateUrl: './edelweiss-pos-home.component.html',
+  styleUrls: ['./edelweiss-pos-home.component.scss']
+})
+export class EdelweissPosHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
