0ac2c72858b6bdb616e15fb4ad304595e898760e
padmadevil <padmamca.94@gail.com>
2019-08-29 15:44:18 +0530

    update

--- vizza/src/app/pages/four-wheeler-home/four-wheeler-home.component.ts
+++ vizza/src/app/pages/four-wheeler-home/four-wheeler-home.component.ts
@@ -189,6 +189,11 @@ export class FourWheelerHomeComponent implements OnInit {
           } else {
             this.dobError = 'Enter Valid Date';
           }
+        } else if (typeof event.value._i == 'object') {
+          if (dob.length == 10) {
+            this.dobError = '';
+
+          }
         }
       }
     }
