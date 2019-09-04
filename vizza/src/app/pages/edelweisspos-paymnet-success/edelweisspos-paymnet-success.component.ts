8ba0ef0404f7e0ec6a79f6be5d92516b2bb00f77
sujay <sujayap7s@gmail.com>
2019-09-04 11:44:58 +0530

    updated

---
+++ vizza/src/app/pages/edelweisspos-paymnet-success/edelweisspos-paymnet-success.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edelweisspos-paymnet-success',
+  templateUrl: './edelweisspos-paymnet-success.component.html',
+  styleUrls: ['./edelweisspos-paymnet-success.component.scss']
+})
+export class EdelweissposPaymnetSuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
