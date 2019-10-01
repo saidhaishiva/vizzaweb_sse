2ac120e567f717072784e7b88a406d36514f7feb
SharmilaDeviS <sharmiladevi5795@gmail.com>
2019-10-01 15:07:13 +0530

    update

--- vizza/src/app/pages/bike-insurance/bike-insurance.component.ts
+++ vizza/src/app/pages/bike-insurance/bike-insurance.component.ts
@@ -206,7 +206,6 @@ export class BikeInsuranceComponent implements OnInit {
     }
 
     getCityLists() {
-        alert('innn')
         const data = {
             'platform': 'web',
             'user_id': this.auth.getPosUserId() ? this.auth.getPosUserId() : '0',
