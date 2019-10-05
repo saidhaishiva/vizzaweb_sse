189edc11e54c629467dc3529f7a038db75af5b04
gnanamanim <chandru9048>
2019-10-05 16:08:35 +0530

    reliance & iffco update

--- vizza/src/app/pages/reliance-heath-proposal/reliance-heath-proposal.component.ts
+++ vizza/src/app/pages/reliance-heath-proposal/reliance-heath-proposal.component.ts
@@ -1280,9 +1280,10 @@ export class RelianceHeathProposalComponent implements OnInit {
                     'DOB': this.datepipe.transform(this.insurerData[i].personalDob, 'y-MM-dd'),
                     'MaritalStatusID': this.insurerData[i].maritalStatus,
                     'maritalStatusName': this.insurerData[i].maritalStatusName,
+                    'occupationName': this.insurerData[i].occupationName,
+                    'occupationname': this.insureArray['controls'].items['controls'][i]['controls'].occupationName.value,
                     'OccupationID': this.insurerData[i].occupation,
                     'personalrelationshipName': this.insurerData[i].personalrelationshipName,
-                    'occupationName': this.insurerData[i].occupationName,
                     'type': this.insurerData[i].type,
                     'PreExistingDisease': {
                         'IsExistingIllness': this.insurerData[i].IsExistingIllness == 'No' ? "false" : "true",
