6b1af078f5c503515770bf0300e5ce707bf8d24d
SharmilaDeviS <sharmiladevi5795@gmail.com>
2019-10-30 10:21:20 +0530

    update

--- vizza/src/app/shared/services/four-wheeler.service.ts
+++ vizza/src/app/shared/services/four-wheeler.service.ts
@@ -868,6 +868,17 @@ export class FourWheelerService {
         .map(this.extractData)
         .catch(this.handleError);
   }
+  packagesList(data) {
+    const json = JSON.stringify(data);
+    const token = this.authService.getAccessToken();
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+    };
+    const url = this.configurationService.getFourwheelerInsurance() + 'tata/addons_contents';
+    return this.http.post(url,json, httpOptions)
+        .map(this.extractData)
+        .catch(this.handleError);
+  }
 
   // // Tataaig NameList
   //
