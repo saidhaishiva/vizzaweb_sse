cdef7d23bfce113aa84c246179e37f94aff6d761
Sughapriya <sughapriyatr16@gmail.com>
2020-01-11 17:57:54 +0530

    update

--- vizza/src/app/pages/health-insurance/health-insurance.component.ts
+++ vizza/src/app/pages/health-insurance/health-insurance.component.ts
@@ -746,7 +746,6 @@ export class HealthInsuranceComponent implements OnInit {
     public getSumInsuredAmountSuccess(successData) {
         console.log(successData.ResponseObject, 'kilo1234kg')
         if (successData.IsSuccess) {
-            alert('1')
             this.sumInsuredAmountLists = successData.ResponseObject;
             console.log(this.sumInsuredAmountLists, 'hello1234')
         }
