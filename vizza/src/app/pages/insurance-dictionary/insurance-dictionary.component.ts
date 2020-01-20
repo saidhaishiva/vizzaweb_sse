fabb68b759168d9ac8b4273eb7932078f332f9c3
keerthanak1506 <keerthanak1506@gmail.com>
2020-01-20 19:44:40 +0530

    commit

--- vizza/src/app/pages/insurance-dictionary/insurance-dictionary.component.ts
+++ vizza/src/app/pages/insurance-dictionary/insurance-dictionary.component.ts
@@ -13,6 +13,7 @@ export class InsuranceDictionaryComponent implements OnInit {
   value: any;
   search: any;
   getinsurancediccontent: any;
+  getinsurancedicsearch: any;
   dictionary:any;
 
   constructor(public learning: LearningcenterService, public auth: AuthService) { }
@@ -20,19 +21,21 @@ export class InsuranceDictionaryComponent implements OnInit {
   ngOnInit() {
     this.value='A';
     this.insurancedic();
+    this.insurancedicSearch();
     this.dictionary=false;
   }
   updateFilter(event) {
     this.search = event.target.value.toLowerCase();
     console.log(this.search, 'val');
+    this.insurancedicSearch();
     this.insurancedic();
 
 
   }
   insurancebuttonclick(event){
     // alert('1')
-    console.log(event,'87345678');
-    console.log(event.target.value,'87345678');
+    // console.log(event,'87345678');
+    // console.log(event.target.value,'87345678');
     this.value= event.target.value;
     this.insurancedic();
 
@@ -60,44 +63,57 @@ export class InsuranceDictionaryComponent implements OnInit {
 
   public getinsurancedicSuccess(successData) {
     if (successData.IsSuccess == true) {
-      this.dictionary=false;
-      this.dictionary='';
+      this.dictionary = false;
+      this.dictionary = '';
       this.getinsurancedicList = successData.ResponseObject;
       console.log(this.getinsurancedicList, 'this.getinsurancedicList');
       this.getinsurancediccontent = successData.ResponseObject.content;
       console.log(this.getinsurancediccontent, 'this.getinsurancediccontent');
-
-    }else{
-      this.dictionary= true;
-      this.dictionary= 'No data found';
-
+      for (let i = 0; i < this.getinsurancedicList.length; i++) {
+        console.log(this.getinsurancedicList.content[i], "12345678");
+        this.insurancedicSearch();
+      }
+    }else {
+      this.dictionary = true;
+      this.dictionary = 'No data found';
 
     }
-
-
-    // for (let i = 0; i < this.faqQuestionsList.length; i++) {
-    //   console.log(this.faqQuestionsList[i].contents, "12345678");
-    //
-    //
-    //   for (let j = 0; j < this.faqQuestionsList[i].contents.length; j++) {
-    //     for(let k = 0; k < this.faqQuestionsList[i].contents[j].content_ans.length; k++){
-    //
-    //     }
-    //     //
-    //     // this.faq_question=this.faqQuestionsList[i].contents[j].content_ans;
-    //     // console.log(this.faq_question, "12345678");
-    //     //
-    //     //
-    //     // this.faq_question=this.faq_question.replace(new RegExp('•', 'g'), "<br _ngcontent-vizza-app-c14>");
-    //     // console.log(this.faq_question, "12345678");
-    //     //
-    //
-    //
-    //
-    //   }
-    // }
   }
 
+
+
   public getinsurancedicFailure(error) {
+  }//search Details
+  insurancedicSearch() {
+    const data = {
+      'platform': 'web',
+      'user_id': this.auth.getPosUserId() ? this.auth.getPosUserId() : '0',
+      'role_id': this.auth.getPosRoleId() ? this.auth.getPosRoleId() : '4',
+      'alphabets':'',
+      'search': this.search
+    }
+    this.learning.getinsurancedicsearch(data).subscribe(
+        (successData) => {
+          this.getinsurancedicsearchSuccess(successData);
+        },
+        (error) => {
+          this.getinsurancedicsearchFailure(error);
+        }
+    );
+
+  }
+
+  public getinsurancedicsearchSuccess(successData) {
+    if (successData.IsSuccess == true) {
+
+      this.getinsurancedicsearch = successData.ResponseObject;
+      console.log(this.getinsurancedicsearch, 'this.getinsurancedicsearch');
+
+    }
+  }
+
+
+
+  public getinsurancedicsearchFailure(error) {
   }
 }
