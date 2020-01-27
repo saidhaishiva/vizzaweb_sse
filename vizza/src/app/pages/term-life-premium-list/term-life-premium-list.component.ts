9ec6c39ed901f0b3064aa352817fb67c33905039
SharmilaDeviS <sharmiladevi5795@gmail.com>
2020-01-27 12:23:26 +0530

    update

--- vizza/src/app/pages/term-life-premium-list/term-life-premium-list.component.ts
+++ vizza/src/app/pages/term-life-premium-list/term-life-premium-list.component.ts
@@ -20,6 +20,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   templateUrl: './term-life-premium-list.component.html',
   styleUrls: ['./term-life-premium-list.component.scss']
 })
+
 export class TermLifePremiumListComponent implements OnInit {
     public settings: Settings;
     allCompanyList: any;
