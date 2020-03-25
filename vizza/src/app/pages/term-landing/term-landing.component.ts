1959bcccdff197a8b80f2bfc4592fada44d0d2ef
llatha7976 <kavyasoun96@gmail.com>
2020-03-25 18:38:54 +0530

    commit

---
+++ vizza/src/app/pages/term-landing/term-landing.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
+import {HealthService} from '../../shared/services/health.service';
+import {ValidationService} from '../../shared/services/validation.service';
+
+@Component({
+  selector: 'app-term-landing',
+  templateUrl: './term-landing.component.html',
+  styleUrls: ['./term-landing.component.scss']
+})
+export class TermLandingComponent implements OnInit {
+  public form: FormGroup;
+  public termLandingSubmit:any;
+
+  constructor(public fb: FormBuilder,public common: HealthService, public toastr: ToastrService , public router: Router, public validation: ValidationService) {
+    this.form = this.fb.group({
+      'firstName': ['', Validators.required],
+      'mobile': ['', Validators.compose([Validators.pattern('[6-9]\\d{9}')])],
+      'emailId': ['', Validators.compose([Validators.required, Validators.pattern("^(([^<>()[\\]\\\\.,;:\\s@\\\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\\\"]+)*)|(\\\".+\\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")])],
+
+
+    });
+  }
+
+  ngOnInit() {
+  }
+  landingSubmit() {
+
+    const data = {
+      "platform":"web",
+      "name" : this.form.controls['firstName'].value,
+      "email_id" : this.form.controls['emailId'].value,
+      "mobile_no": this.form.controls['mobile'].value,
+      "city": "fftfty",
+      "insurance_type":"term"
+    }
+    this.common.healthLandingProposal(data).subscribe(
+        (successData) => {
+          this.termLandingSuccess(successData);
+        },
+        (error) => {
+          this.termLandingFailure(error);
+        }
+    );
+  }
+  public termLandingSuccess(successData) {
+    if (successData.IsSuccess == true) {
+      this.termLandingSubmit =  successData.ResponseObject;
+      this.toastr.success(this.termLandingSubmit);
+      this.router.navigate(['/home']);
+    } else {
+      this.toastr.error(successData.ErrorObject);
+    }
+
+
+  }
+  public termLandingFailure(error) {
+  }
+
+  //validation
+  nameValidate(event: any) {
+    this.validation.nameValidate(event);
+  }
+
+  numberValidate(event: any) {
+    this.validation.numberValidate(event);
+  }
+}
