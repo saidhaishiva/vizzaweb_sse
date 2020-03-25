227a697ecf2a9634b4b9c22af50391ab3072cc63
llatha7976 <kavyasoun96@gmail.com>
2020-03-25 10:46:49 +0530

    commit

--- vizza/src/app/pages/health-landing/health-landing.component.ts
+++ vizza/src/app/pages/health-landing/health-landing.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CommonService} from '../../shared/services/common.service';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService} from 'ngx-toastr';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {ConfigurationService} from '../../shared/services/configuration.service';
+import {Settings} from '../../app.settings.model';
+import {AppSettings} from '../../app.settings';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material';
+import {MetaService} from '../../shared/services/meta.service';
+import {AuthService} from '../../shared/services/auth.service';
+import {Meta, Title} from '@angular/platform-browser';
+import { WINDOW } from '@ng-toolkit/universal';
+
 
 @Component({
   selector: 'app-health-landing',
@@ -6,8 +23,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./health-landing.component.scss']
 })
 export class HealthLandingComponent implements OnInit {
+public form: FormGroup
+  constructor(public fb: FormBuilder) {
+    this.form = this.fb.group({
+      'firstName': ['', Validators.required],
+      'mobile': ['', Validators.required],
+      'email': ['', Validators.required],
 
-  constructor() { }
+    });
+  }
 
   ngOnInit() {
   }
