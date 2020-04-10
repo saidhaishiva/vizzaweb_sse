3b607f2b8a340d4a7dbf89119f456c2ff76f94fd
SharmilaDeviS <sharmiladevi5795@gmail.com>
2020-04-10 19:12:00 +0530

    update

---
+++ vizza/src/app/pages/star-covid-proposal/star-covid-proposal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-star-covid-proposal',
+  templateUrl: './star-covid-proposal.component.html',
+  styleUrls: ['./star-covid-proposal.component.scss']
+})
+export class StarCovidProposalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
