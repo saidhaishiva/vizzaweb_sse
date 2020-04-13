cb1af742e31ba28a3249db760c59f03f53c3115a
Sughapriya <sughapriyatr16@gmail.com>
2020-04-13 18:09:05 +0530

    updates

--- vizza/src/app/pages/pos-mock-test/pos-mock-test.component.ts
+++ vizza/src/app/pages/pos-mock-test/pos-mock-test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-pos-mock-test',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pos-mock-test.component.scss']
 })
 export class PosMockTestComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
+
+  constructor(public fb: FormBuilder) {
+    this.form = this.fb.group({
+      // 'username': ['', Validators.compose([Validators.required, Validators.minLength(10)])],
+      // 'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])]
+      'name': [''],
+      'email': [''],
+      'mobile': ['']
+    });
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+console.log('hello');
   }
 
 }
